7fd6df6e8253eb1b80788f2c4463c955caee761e
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-05-22 18:36:31 +0200

    ADD: loop in comics page showing a card with picture, title and series for every iteration of the comics array

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <section>
+    <section class="py-5">
         <div class="container">
             <h1>COMICS</h1>
+            <div class="row gy-3 mt-3">
+                @foreach ($comics as $comic)
+                <div class="col-2">
+                    <div class="card">
+                        <img src="{{$comic['thumb']}}" class="card-img-top">
+                        <div class="card-body">
+                          <h5 class="card-title">{{$comic['title']}}</h5>
+                          <h5 class="card-subtitle mb-2">{{$comic['series']}}</h5>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
         </div>
     </section>
 @endsection
