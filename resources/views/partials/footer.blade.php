c739a8d7749bceaea62331659a6a95c61af71fdc
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-05-22 17:39:57 +0200

    ADD: footer style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,8 @@
-<footer>
+<footer class="page-footer">
     <div class="container">
-        <p>Made by Mary</p>
+        <div class="d-flex align-items-center justify-content-between py-4">
+            <button type="button" class="btn btn-outline-primary">SIGN UP NOW!</button>
+            <p>Made by Mary</p>
+        </div>
     </div>
 </footer>
